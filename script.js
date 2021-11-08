c3aeade35d1ba85aa0fecd4033b6f0c07986c04e
shruti325 <sharmashruti71930@gmail.com>
2021-11-08 12:34:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,153 @@
+
+let C4 = document.getElementsByClassName("C4")
+
+Array.from(C4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/C4.mp3').play()        
+    })
+})
+
+let Db4= document.getElementsByClassName("Db4")
+
+Array.from(Db4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Db4.mp3').play()        
+    })
+})
+
+let D4= document.getElementsByClassName("D4")
+
+Array.from(D4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/D4.mp3').play()        
+    })
+})
+
+let Eb4= document.getElementsByClassName("Eb4")
+
+Array.from(Eb4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Eb4.mp3').play()        
+    })
+})
+
+let E4= document.getElementsByClassName("E4")
+
+Array.from(E4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/E4.mp3').play()        
+    })
+})
+
+let F4= document.getElementsByClassName("F4")
+
+Array.from(F4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/F4.mp3').play()        
+    })
+})
+
+let Gb4= document.getElementsByClassName("Gb4")
+
+Array.from(Gb4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Gb4.mp3').play()        
+    })
+})
+
+let G4= document.getElementsByClassName("G4")
+
+Array.from(G4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/G4.mp3').play()        
+    })
+})
+
+let Ab4= document.getElementsByClassName("Ab4")
+
+Array.from(Ab4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Ab4.mp3').play()        
+    })
+})
+
+let A4= document.getElementsByClassName("A4")
+
+Array.from(A4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/A4.mp3').play()        
+    })
+})
+
+let Bb4= document.getElementsByClassName("Bb4")
+
+Array.from(Bb4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Bb4.mp3').play()        
+    })
+})
+
+let B4= document.getElementsByClassName("B4")
+
+Array.from(B4).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/B4.mp3').play()        
+    })
+})
+
+let C5= document.getElementsByClassName("C5")
+
+Array.from(C5).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/C5.mp3').play()        
+    })
+})
+
+let Db5= document.getElementsByClassName("Db5")
+
+Array.from(Db5).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Db5.mp3').play()        
+    })
+})
+
+let D5= document.getElementsByClassName("D5")
+
+Array.from(D5).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/D5.mp3').play()        
+    })
+})
+
+let Eb5= document.getElementsByClassName("Eb5")
+
+Array.from(Eb5).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("black clicked")
+        new Audio('./audio/Eb5.mp3').play()        
+    })
+})
+
+let E5= document.getElementsByClassName("E5")
+
+Array.from(E5).forEach((e)=>{
+    e.addEventListener('click',()=>{
+        console.log("white clicked")
+        new Audio('./audio/E5.mp3').play()        
+    })
+})
